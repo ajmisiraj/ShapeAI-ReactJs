fe41b09ef0296b62ca467b97d63647984443dd33
ajmisiraj <69889142+ajmisiraj@users.noreply.github.com>
2021-06-24 21:12:36 +0530

    Initial commit

---
+++ src/components/Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A Basic Web Dev REact Js Bootcamp </p>
+    </div>
+  );
+}
+export default Info;
